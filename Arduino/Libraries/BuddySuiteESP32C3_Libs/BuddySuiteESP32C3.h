8b9d6b7fbe605badaf760018f5bad7c6b7d0109d
Minty Love <mintylove45a@gmail.com>
2025-11-25 21:58:07 +0000

    Update BuddySuiteESP32C3.h

--- Arduino/Libraries/BuddySuiteESP32C3_Libs/BuddySuiteESP32C3.h
+++ Arduino/Libraries/BuddySuiteESP32C3_Libs/BuddySuiteESP32C3.h
@@ -62,19 +62,25 @@ class JB_LcdHdlr
 		void setSelectedField(uint8_t sel); //0-dJuice Reqd, 1-dRatio g/ml, 2-PG/VG, 3-DMT
 		void setJoystickMeter(uint16_t & rawXPos);  //0-4096
 private:
-		const uint8_t xMarginLeftPixels   = 12;
-		const uint8_t xMarginRightPixels  = 12;
-		const uint8_t lineSpacing	= 30;
+		const uint8_t xMarginLeftPixels   = 12 ;
+		const uint8_t xMarginRightPixels  = 12 ;
+		const uint8_t xOffsetMg 		  = 148;
+		const uint8_t xOffsetMl           = 150;
+		const uint8_t lineSpacing		  = 30;
 		const uint8_t textToUnitGapPixels = 3 ; //eg 1.3<gap>ml
+		uint16_t 	  olubleStrLenPixels  = 0 ; //set in setupScreen()
 		const String  mlStr 		= "ml"; 
+		const String  olubleStr     = "oluble";
 		uint16_t colorTextBG    	= 0x0004; // 5.6.5 RGB
 		uint32_t colorScreenBG  	= ST77XX_CYAN;
 		uint16_t colorText1     	= ST77XX_YELLOW;
 		uint16_t colorText2     	= 0X7E0;
 		uint32_t colorCursor   	 	= 0xFAAA;  
-		float 	oldTojMl            = 0;
-		float 	oldDeemsRatio 		= 0; 
-		int16_t oldPgRatio	 		= 0;		
+		float 	 oldTojMl           = 0;
+		float 	 oldDeemsRatio 		= 0; 
+		int16_t  oldPgRatio	 		= 0;
+		uint16_t oldDeemsMg			= 0;
+        uint16_t oldTotalWeightOfDJuice_g = 0;		
 		void setupScreen();
 		void setDjuiceRequiredField	(CurrentValuesJB & values);
 		void setDRatField			(CurrentValuesJB & values);
