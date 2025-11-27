fbf4f98178f8fc03ac0e195204d68164ecf0a150
Minty Love <mintylove45a@gmail.com>
2025-11-27 22:07:21 +0000

    Update BuddySuiteESP32C.ino

--- Arduino/BuddySuiteESP32C/BuddySuiteESP32C.ino
+++ Arduino/BuddySuiteESP32C/BuddySuiteESP32C.ino
@@ -1,5 +1,5 @@
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+//27Nov2025
+#include <SPI.h>
 #include <BuddySuiteESP32C3.h>
 //#include <./Pics/picMinty.h>
 //#include <./Pics/picDestiny.h>
@@ -7,82 +7,24 @@
 #include <./fonts/Open_Sans_Italic_21.h> 
 #include <./fonts/Open_Sans_Italic_16.h> 
 #include <./fonts/Open_Sans_Italic_23.h> 
-
-#define TFT_CS        21
-#define TFT_RST       9 // Or set to -1 and connect to Arduino RESET pin
-#define TFT_DC        10
-// gpio6 is mosi (SDA on display)
-// gpio4 is sck  (SCL on display)
-//For all connections, see "Utils" tab
-  
+//For ESP -> ST7789 TFT connections, see "Utils" tab
 Utils utils;
-CurrentValuesJB valuesJB;
-JB_Calc jb_calc;
-JB_LcdHdlr jb_lcdHandler;
+JB_Main jbMain;
+
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("here1");
-  jb_lcdHandler.setup();
-  jb_calc.setup();
-  valuesJB.setup();
-  jb_calc.calculate(valuesJB, false);
-  jb_lcdHandler.updateScreen(valuesJB);
- 
-  // SPI speed defaults to SPI_DEFAULT_FREQ defined in the library, you can override it here
-  // Allowable spped depends on chip & wiring Too fast may get you a black screen some times, continuous.
-  //tft.setSPISpeed(40000000);
-  //delay(5000);
-}
-
-void printValues()
-{
-	Serial.println (String("TotalJuice Reqd=") + String(valuesJB.getTojMl()));
-	Serial.println (String("Deems Ratio=1:") + String(valuesJB.deemsRatio));
-  Serial.println (String("PG/VG=")+ String(valuesJB.pgRatio) + String("/") 		+ String ((100-valuesJB.pgRatio)));
-  Serial.println (String("PG=") 	+ String(valuesJB.pgMl) 	 + String(", (") 	+ String (valuesJB.weightPg_g) + String("g)"));
-  Serial.println (String("VG=") 	+ String(valuesJB.vgMl) 	 + String(", (") 	+ String (valuesJB.weightVg_g) + String("g)"));
-  Serial.println (String("Total juice Weight=") + String(valuesJB.totalWeightOfDJuice_g));
-  Serial.println (String("Solubility=") + String(valuesJB.solubility));
+   jbMain.initJB();
+   jbMain.startJB();
 }
 
 void loop() 
 {
-	test();
+	jbMain.test();
   delay(5000);
 }
 
-void test()
-{
-//PG Ratio test
-for (int16_t f=30; f>-1; f-=1)
-	{
-			valuesJB.pgRatio=f;
-		 	jb_calc.calculate(valuesJB, false);
-     	jb_lcdHandler.updateScreen(valuesJB);
-     	delay (200);
-	}
 
-/**
-////dratio test
-for (float f=0; f<21; f+=0.5)
-	{
-			valuesJB.deemsRatio=f;
-		 	jb_calc.calculate(valuesJB, false);
-     	jb_lcdHandler.updateScreen(valuesJB);
-     	delay (100);
-	}
-*/
-	//djuice req'd test
-	for (float f=0; f<90; f+=0.9)
-	{
-			valuesJB.setTojMl(f);
-		 	jb_calc.calculate(valuesJB, false);
-     	jb_lcdHandler.updateScreen(valuesJB);
-     	delay (100);
-	}
-	
-}
 
 //void testlines(uint16_t color) {
 //  tft.fillScreen(ST77XX_BLACK);
