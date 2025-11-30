bb3858e442c348971cb20d9ce3bc7dab2505fd07
Minty Love <mintylove45a@gmail.com>
2025-11-30 20:28:13 +0000

    Update LcdHdlr.ino

--- Arduino/BuddySuiteESP32C/LcdHdlr.ino
+++ Arduino/BuddySuiteESP32C/LcdHdlr.ino
@@ -1,19 +1,25 @@
-//28Nov2025
-void LcdHdlr::setup(){tft.init(240, 320);}  //2.0" 320x240 ST7789 TFT}
+//30Nov2025
+void LcdHdlr::setup()
+{
+	tft.init(240, 320);
+	showStartupSplash();
+}  //2.0" 320x240 ST7789 TFT}
 
 void LcdHdlr::showStartupSplash()
 {
-				
+		drawBitMap(tft);//2 secdelay built in		
 }
-
+#include <./Pics/splash1.h>
 void LcdHdlr::drawBitMap(Adafruit_ST7789 tft)
 	{
 				tft.fillScreen(ST77XX_BLACK);
-				//tft.drawRGBBitmap(0, 0, bitmap_Minty320_170, 320,170);
-				delay (10000);
-				tft.fillScreen(ST77XX_BLACK);
+				tft.setRotation(1);
+				tft.drawRGBBitmap(0, 0, splash1, 320,240);
+				delay (2000);
+				
+				//tft.fillScreen(ST77XX_BLACK);
 				//tft.drawRGBBitmap(0, 0, bitmap_DestinyVideo320_170, 320,170);
-				delay (10000);
+				//delay (10000);
 				tft.fillScreen(ST77XX_BLACK);
 			   //tft.drawRGBBitmap(0, 0, lcd_gs850, 320,215);
 				/**Many more drawbitmap functs in Adafruit_gfx.h
@@ -23,4 +29,3 @@ void LcdHdlr::drawBitMap(Adafruit_ST7789 tft)
 				void drawRGBBitmap(int16_t x, int16_t y, uint16_t *bitmap, uint8_t *mask, int16_t w, int16_t h);
 				*/
 	}
-
