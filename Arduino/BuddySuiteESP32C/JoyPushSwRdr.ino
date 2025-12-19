bbf352f7453d367760efb0795b256b1b1eb00368
Minty Love <mintylove45a@gmail.com>
2025-12-19 13:55:46 +0000

    Add files via upload

---
+++ Arduino/BuddySuiteESP32C/JoyPushSwRdr.ino
@@ -0,0 +1,41 @@
+//19Dec2025
+boolean OldJoyPushSwitchStatus           = 1;
+void JoystickPushSwitchReader::setup()
+{
+  pinMode(JOYSTICK_PUSHSW_PIN, INPUT_PULLUP);   
+}
+
+void JoystickPushSwitchReader::checkForSwitchPush()
+{
+  boolean joyPushSwitchStatus = digitalRead(JOYSTICK_PUSHSW_PIN);
+  if (!OldJoyPushSwitchStatus && joyPushSwitchStatus)
+  {
+     sendPushSwitchNotification();
+  }
+  OldJoyPushSwitchStatus=joyPushSwitchStatus;
+}
+
+void JoystickPushSwitchReader::sendPushSwitchNotification()
+{
+  if (jb_lcdHandler.screenShowing==JB) //{JB, JBSAV};
+  {   
+      jb_SaveScreenHndlr.setupScreen(valuesJB); //draws background static graphics
+      joystickReaderJBSAV.setup(); //oldJoystickXRaw=joystickCentreXRaw and  oldJoystickYRaw=joystickCentreYRaw;
+  }    
+  else if(jb_lcdHandler.screenShowing==JBSAV)
+  {
+    if(rowSelectedJBSAV==0)
+    {
+      jb_lcdHandler.setupScreen();
+      jb_lcdHandler.setSelectedField(rowSelectedJB);
+      jb_calc.calculate             (valuesJB, false);
+      jb_lcdHandler.updateScreen    (valuesJB, true);  
+    } 
+    else
+    {
+      jb_SaveScreenHndlr.doClick();
+      rowSelectedJBSAV=0;
+      jb_SaveScreenHndlr.setSelectedRow(0);
+    }
+  }
+}
