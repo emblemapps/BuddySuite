9c328bb411c691393f498a4768248e900648062d
Minty Love <mintylove45a@gmail.com>
2026-01-03 12:42:02 +0000

    Update BuddySuiteESP32C.ino

--- Arduino/BuddySuiteESP32C/BuddySuiteESP32C.ino
+++ Arduino/BuddySuiteESP32C/BuddySuiteESP32C.ino
@@ -1,11 +1,16 @@
-//26Dec2025
+//03Jan2026
 #include <SPI.h>
 #include <BuddySuiteESP32C3.h>
 #include <./fonts/Open_Sans_Condensed_Italic_19.h>
 #include <./fonts/Open_Sans_Italic_23.h> 
 #include <./fonts/Open_Sans_Italic_21.h>
 #include <./fonts/Roboto_Condensed_Italic_17.h>
+#include <./fonts/DSEG14_Modern_Bold_Italic_17.h>  
 //For ESP -> ST7789 TFT connections, see "Utils" tab
+#include <./overflowJB/Utils.cpp>
+#include <./overflowJB/LittleFSManager.cpp>
+#include <./overflowJB/JB_Calc.cpp>
+//#define INVERT_DISPLAY 0
 Utils utils;
 JB_Main jbMain;
 LittleFSManager littleFS;
